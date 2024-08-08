9f0179b44f0c661add689b40fb918171cf52864a
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-09 04:45:43 +0700

    Menambahkan file belajar Boolean

---
+++ bin/1. Dart Dasar/boolean.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Belajar tipe data Boolean
+  bool finish = false;
+  print(finish);
+
+  finish = true;
+  // finish = benar; //ini akan menjadi error
+  print(finish);
+  print(finish.runtimeType);
+}
