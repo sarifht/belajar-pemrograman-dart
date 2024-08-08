75eae8d51f97c9f6c9d2c750a92325f1e63f7e1f
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-09 04:50:17 +0700

    Menambahkan string dan string interpolation

---
+++ bin/1. Dart Dasar/string.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Belajar tipe data String
+  String firstName = 'Sarif';
+  String lastName = 'Hidayatullah';
+
+  print(firstName);
+  print(lastName);
+
+  // String Interpolation
+  var fullName = '$firstName ${lastName}';
+  print(fullName);
+}
