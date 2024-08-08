c92f3fb57c8749e4187e55530e48c8f64e50e1ab
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-08 18:34:50 +0700

    Menambahkan komentar pada kodingan file comment.dart

--- bin/1. Dart Dasar/comment.dart
+++ bin/1. Dart Dasar/comment.dart
@@ -1,1 +1,12 @@
+/// Ini contoh komentar untuk dokumentasi
 
+void main() {
+  // Ini contoh komentar satu baris
+  String name = 'Sarif Hidayatullah';
+
+  /*
+    Ini contoh komentar multi baris
+    Komentar ini bisa digunakan untuk penjelasan yang lebih panjang
+  */
+  print('Komentar satu baris tidak dieksekusi, ditulis oleh: $name');
+}
