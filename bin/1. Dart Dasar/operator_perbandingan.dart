2fd8aeeb438fd7e70017cd7586a011f8b43665a8
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-11 13:40:12 +0700

    Add file operator_perbandingan.dart

---
+++ bin/1. Dart Dasar/operator_perbandingan.dart
@@ -0,0 +1,19 @@
+void main() {
+  // Operator Perbandingan
+  var angka1 = 10;
+  var angka2 = 3;
+
+  print(angka1 == angka2);
+  print(angka1 != angka2);
+  print(angka1 > angka2);
+  print(angka1 < angka2);
+  print(angka1 >= angka2);
+  print(angka1 <= angka2);
+
+  // Perbandingan String
+  print('');
+  print('sarif' == 'sarif');
+  print('sarif' == 'Sarif');
+  print(10 == '10');
+  print('sarif' != 'sarif');
+}
