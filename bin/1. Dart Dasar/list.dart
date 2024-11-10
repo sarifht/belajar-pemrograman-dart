554d22b15caab489e64d905a6e8c0c96f3eecee6
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-11-10 19:27:23 +0700

    Add cara pemanggilan data index ke- .. di List

--- bin/1. Dart Dasar/list.dart
+++ bin/1. Dart Dasar/list.dart
@@ -4,4 +4,14 @@ void main() {
 
   print(numberList);
   print(stringList);
+
+  var names = ['Sarif', 'Hidayatullah'];
+  print(names);
+
+  var namaLengkap = [];
+
+  namaLengkap.add('Kamu');
+  namaLengkap.add('Miskin');
+  print(namaLengkap);
+  print(namaLengkap[0] + " " + namaLengkap[1]);
 }
