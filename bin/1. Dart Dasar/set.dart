01bfa40c361b1711c494bdc55b02eef372837271
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-11-11 07:52:43 +0700

    Add file set.dart

---
+++ bin/1. Dart Dasar/set.dart
@@ -0,0 +1,24 @@
+void main() {
+  Set<int> angka = {1, 1, 2, 3, 4};
+  print(angka);
+
+  var names = <String>{};
+
+  names.add('Sarif');
+  names.add('Hidayatullah');
+  names.add('Kamu');
+  names.add('Sarif');
+
+  print(names);
+
+  names.add('1');
+  print(names.length);
+
+  var city = <String>{
+    "Bandung",
+    "Jakarta",
+    "Batam",
+  };
+
+  print(city);
+}
