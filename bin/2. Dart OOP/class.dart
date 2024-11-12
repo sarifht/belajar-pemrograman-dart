a09de923c7e14f86b87a68b67088d420f594b2cf
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-11-12 19:39:54 +0700

    Update class.dart belajar field

--- bin/2. Dart OOP/class.dart
+++ bin/2. Dart OOP/class.dart
@@ -1,8 +1,20 @@
-class Person {}
+class Person {
+  String name = "Tamu";
+  String? address;
+  final String country = "Indonesia";
+}
 
 void main() {
   var person1 = Person();
-  print(person1);
+
+  // Mencoba mengubah data object di class Person
+  person1.name = "Sarif Hidayatullah";
+  person1.address = "Batam";
+  // Tidak bisa mengubah country karen data Final
+
+  print(person1.name);
+  print(person1.address);
+  print(person1.country);
 
   Person person2 = Person();
   print(person2);
