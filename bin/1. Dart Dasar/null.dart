221a9bdd38787c0e4658887bd6d23921fe38ea60
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-11-23 23:28:10 +0700

    Add kodingan null.dart

---
+++ bin/1. Dart Dasar/null.dart
@@ -0,0 +1,15 @@
+void main() {
+  // int number1;
+  // Variable number1 akan error, karena tidak isinya
+
+  int number1 = 15;
+  // int number1 = null;
+  // Variable diatas juga akan error
+  // Karena sebelumnya number1, sudah ada nilainya
+
+  int? number2;
+  number2 = null;
+
+  print(number1);
+  print(number2);
+}
